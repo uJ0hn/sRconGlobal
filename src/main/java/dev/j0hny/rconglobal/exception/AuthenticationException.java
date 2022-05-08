9475fbace66f8d6c6776987a4dc0da946a016752
uJ0hn <pessoalmurillofrazao@gmail.com>
2022-05-08 16:08:01 -0300

    Released version ARTIFACT-VERSION

--- src/main/java/dev/j0hny/rconglobal/exception/AuthenticationException.java
+++
@@ -1,8 +0,0 @@
-package dev.j0hny.rconglobal.exception;
-
-public class AuthenticationException extends Exception {
-
-    public AuthenticationException(String message) {
-        super(message);
-    }
-}
